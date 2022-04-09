eb06743676f9b8e62a504f44ddedf4b081a8bafa
nozomi <x8ojo8x@gmail.com>
2022-04-09 23:39:37 +0900

    bowling.rb commit

---
+++ 04.bowling/bowling.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+score = ARGV[0]
+scores = score.chars
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+  else
+    shots << s.to_i
+  end
+end
+
+frame = 0
+max_frame = 10
+count = 0
+score = [0] * max_frame
+
+shots.length.times do |i|
+  if frame + 1 != max_frame
+    if shots[i] == 10
+      if count.zero?
+        count = 1
+        score[frame] = shots[i] + shots[i + 1] + shots[i + 2]
+      else
+        score[frame] = shots[i] + shots[i + 1]
+      end
+
+    elsif shots[i] + score[frame] == 10
+      score[frame] = score[frame] + shots[i] + shots[i + 1]
+    else
+      score[frame] += shots[i]
+    end
+    count += 1
+    if count == 2
+      count = 0
+      frame += 1
+    end
+
+  else
+    score[frame] += shots[i]
+  end
+end
+
+puts score.sum
