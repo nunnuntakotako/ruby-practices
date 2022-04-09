182aacc1713098aa24036476874d459105c9ff99
nozomi <x8ojo8x@gmail.com>
2022-04-10 08:52:32 +0900

    bowling.rb Second commit

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -4,11 +4,11 @@ score = ARGV[0]
 scores = score.chars
 shots = []
 scores.each do |s|
-  if s == 'X'
-    shots << 10
-  else
-    shots << s.to_i
-  end
+  shots << if s == 'X'
+             10
+           else
+             s.to_i
+           end
 end
 
 frame = 0
