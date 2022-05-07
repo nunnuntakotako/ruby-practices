1758408f94eeffb71f28b53a0b2980eeb5cff7d9
nozomi <x8ojo8x@gmail.com>
2022-05-07 09:41:44 +0900

    ls2.rb second commit

--- 05.ls/ls2.rb
+++ 05.ls/ls2.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'optparse'
 
 COLUMN = 3
@@ -6,12 +7,10 @@ COLUMN = 3
 def arrangement
   contents = Dir.glob('*')
   opt = OptionParser.new
-  opt.on('-a'){
-    contents = Dir.glob("*", File::FNM_DOTMATCH)
-  }
+  opt.on('-a') { contents = Dir.glob('*', File::FNM_DOTMATCH) }
   opt.parse(ARGV)
   quantity = contents.length.to_f
-  row = ( quantity / COLUMN ).ceil
+  row = (quantity / COLUMN).ceil
   view = contents.each_slice(row).to_a
 
   view[-1] << nil while view[-1].size < row
