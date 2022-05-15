ec289cd11c4b9b6bab823272eda16662b2af1f13
nozomi <x8ojo8x@gmail.com>
2022-05-15 23:45:54 +0900

    lsコマンド3の提出

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -5,10 +5,9 @@ require 'optparse'
 COLUMN = 3
 
 def arrangement
-  options = ARGV.getopts('a')
+  options = ARGV.getopts('r')
   args = ['*']
-  args << File::FNM_DOTMATCH if options['a']
-  contents = Dir.glob(*args)
+  contents = options['r'] ? Dir.glob(*args).reverse : Dir.glob(*args)
   quantity = contents.length.to_f
   row = (quantity / COLUMN).ceil
   view = contents.each_slice(row).to_a
