616ca239a8c6a0a0c718084e6e3b7103c9268a96
nozomi <x8ojo8x@gmail.com>
2022-05-26 15:08:01 +0900

    lsコマンド4再提出

--- 05.ls/ls1.rb
+++ 05.ls/ls1.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'etc'
 require 'optparse'
 
@@ -6,15 +8,13 @@ COLUMN = 3
 options = ARGV.getopts('l')
 
 def l_options
-  arrangement_l = []
   contents = Dir.glob('*')
-  contents.each do |file|
+  arrangement_l = contents.map do |file|
     file_inf = File.mtime(file)
     name = File.basename(file)
     stat = File.stat(file)
     user = Etc.getpwuid(stat.uid).name
     group = Etc.getgrgid(stat.gid).name
-
     file_permission = stat.mode.to_s(8)
 
     file_type = if file_permission.length >= 6
@@ -24,51 +24,58 @@ def l_options
                 end
 
     type = ''
-    case file_type[0].to_i
-    when 10
-      type = 'p'
-    when 20
-      type = 'c'
-    when 40
-      type = 'd'
-    when 60
-      type = 'b'
-    when 100
-      type = '-'
-    when 120
-      type = 'l'
-    when 140
-      type = 's'
-    end
+    type_categorize(file_type)
+    rwx_categorize(file_type, type)
 
-    permission = []
-    file_type[1].chars.map(&:to_i).each do |types|
-      permission << types.to_s(2)
-    end
-
-    permissions = []
-    permission.each do |per|
-      permissions << per.chars.map(&:to_i)
-    end
-
-    permissions.each do |num|
-      (0..num.size - 1).each do |n|
-        if n.zero? && num[n] == 1
-          type.concat('r')
-        elsif n == 1 && num[n] == 1
-          type.concat('w')
-        elsif n == 2 && num[n] == 1
-          type.concat('x')
-        else
-          type.concat('-')
-        end
-      end
-    end
-
-    arrangement_l << "#{type}  #{stat.nlink} #{user}  #{group}  #{stat.size}  #{file_inf.mon} #{file_inf.mday} #{file_inf.strftime('%H:%M')} #{name}"
+                  "#{type}  #{stat.nlink} #{user}  #{group}  #{stat.size}  #{file_inf.mon} #{file_inf.mday} #{file_inf.strftime('%H:%M')} #{name}"
   end
 
   arrangement_l(arrangement_l)
+end
+
+def type_categorize(file_type)
+  case file_type[0].to_i
+  when 10
+    type.to_s = 'p'
+  when 20
+    type.to_s = 'c'
+  when 40
+    type.to_s = 'd'
+  when 60
+    type.to_s = 'b'
+  when 100
+    type.to_s = '-'
+  when 120
+    type.to_s = 'l'
+  when 140
+    type.to_s = 's'
+  end
+end
+
+def rwx_categorize(file_type, type)
+  permission = []
+  file_type[1].chars.map(&:to_i).each do |types|
+    permission << types.to_s(2)
+  end
+
+  permissions = []
+  permission.each do |per|
+    permissions << per.chars.map(&:to_i)
+  end
+
+  permissions.each do |num|
+    (0..num.size - 1).each do |n|
+      type += if n.zero? && num[n] == 1
+                'r'
+              elsif n == 1 && num[n] == 1
+                'w'
+              elsif n == 2 && num[n] == 1
+                'x'
+              else
+                '-'
+              end
+    end
+  end
 end
 
 def arrangement_l(arrangement_l)
@@ -91,7 +98,7 @@ def output(view)
   end
 end
 
-def arrangement
+def non_options
   contents = Dir.glob('*')
   quantity = contents.length.to_f
   row = (quantity / COLUMN).ceil
@@ -105,5 +112,5 @@ end
 if options['l']
   l_options
 else
-  arrangement
+  non_options
 end
