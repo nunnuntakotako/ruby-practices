d9fa68f92db170b1293e2b18d48b45374ce4e6d7
nozomi <x8ojo8x@gmail.com>
2022-08-05 22:54:58 +0900

    wcコマンドの提出3

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,42 +3,33 @@
 require 'optparse'
 
 options = ARGV.getopts('l')
-file_information = []
-input_contents = (!ARGF.argv.count.zero? ? ARGF.argv : readlines)
-total = [0, 0, 0, 'total']
 
-input_contents.each do |content|
-  str = content.include?("\n") ? content : File.read(content)
-  ary = str.split(/\s+/)
-  file_information << [
-    str.lines.count,
-    ary.size,
-    content ? str.bytesize : FileTest.size(content),
-    File.basename(content)
-  ]
-end
+input_contents = (!ARGF.argv.count.zero? ? ARGF.argv : [$stdin.read])
+total_count = 0
+total_word = 0
+total_size = 0
 
-if input_contents == ARGF.argv
-  file_information.each do |file|
-    row = options['l'] ? "      #{file[0]}   #{file[3]}\n" : "      #{file[0]}"
-    print row
-    print "      #{file[1]}       #{file[2]}   #{file[3]} \n" unless options['l']
+MAX_WIDTH = 8
+
+input_contents.each do |input_content|
+  content = (input_content.include?("\n") ? input_content : File.read(input_content))
+  print content.count("\n").to_s.rjust(MAX_WIDTH)
+  unless options['l']
+    print content.split(/\s+/).size.to_s.rjust(MAX_WIDTH)
+    print content.bytesize.to_s.rjust(MAX_WIDTH)
   end
+  print "#{input_content.to_s.rjust(MAX_WIDTH)}\n" unless input_content.include?("\n")
+
+  total_count += content.count("\n")
+  total_word += content.split(/\s+/).size
+  total_size += content.bytesize
 end
 
-file_information.each do |file|
-  total[0] += file[0]
-  total[1] += file[1]
-  total[2] += file[2]
-end
-
-if file_information.size > 1
-  if input_contents == readlines
-    row_total = (options['l'] ? "      #{total[0]}   #{total[3]}\n" : "      #{total[0]}")
-    print row_total
-    print "      #{total[1]}     #{total[2]}     #{total[3]}" unless options['l']
+if input_contents.length > 1
+  print total_count.to_s.rjust(MAX_WIDTH)
+  unless options['l']
+    print total_word.to_s.rjust(MAX_WIDTH)
+    print total_size.to_s.rjust(MAX_WIDTH)
   end
-  print "      #{total[0]}"
-  print "      #{total[1]}     #{total[2]}" unless options['l']
-  print "      #{total[3]}" if input_contents == ARGF.argv
+  print '    total'
 end
