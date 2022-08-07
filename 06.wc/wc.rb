2a6691aecc2957dae3cd69409920a71ebc27dfff
nozomi <x8ojo8x@gmail.com>
2022-08-07 22:18:40 +0900

    wcコマンドinput_contentsの処理修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,25 +4,32 @@ require 'optparse'
 
 options = ARGV.getopts('l')
 
-input_contents = (!ARGF.argv.count.zero? ? ARGF.argv : [$stdin.read])
+input_contents = (ARGF.argv.count.zero? ? [$stdin.read] : ARGF.argv)
+contents = []
 total_count = 0
 total_word = 0
 total_size = 0
-
 MAX_WIDTH = 8
 
-input_contents.each do |input_content|
-  content = (input_content.include?("\n") ? input_content : File.read(input_content))
-  print content.count("\n").to_s.rjust(MAX_WIDTH)
-  unless options['l']
-    print content.split(/\s+/).size.to_s.rjust(MAX_WIDTH)
-    print content.bytesize.to_s.rjust(MAX_WIDTH)
-  end
-  print "#{input_content.to_s.rjust(MAX_WIDTH)}\n" unless input_content.include?("\n")
+input_contents.each do |name|
+  contents << if name.include?("\n")
+                [nil, name]
+              else
+                [name, File.read(name)]
+              end
+end
 
-  total_count += content.count("\n")
-  total_word += content.split(/\s+/).size
-  total_size += content.bytesize
+contents.each do |content|
+  print content[1].count("\n").to_s.rjust(MAX_WIDTH)
+  unless options['l']
+    print content[1].split(/\s+/).size.to_s.rjust(MAX_WIDTH)
+    print content[1].bytesize.to_s.rjust(MAX_WIDTH)
+  end
+  print "#{content[0].to_s.rjust(MAX_WIDTH)}\n" unless content[0] == nil?
+
+  total_count += content[1].count("\n")
+  total_word += content[1].split(/\s+/).size
+  total_size += content[1].bytesize
 end
 
 if input_contents.length > 1
@@ -31,5 +38,5 @@ if input_contents.length > 1
     print total_word.to_s.rjust(MAX_WIDTH)
     print total_size.to_s.rjust(MAX_WIDTH)
   end
-  print '    total'
+  print 'total'.to_s.rjust(MAX_WIDTH)
 end
